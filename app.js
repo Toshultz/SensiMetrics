808676bcfcc2ca5b529887d99749738944429058
Toshultz <toshultz@gmail.com>
2016-01-21 21:21:09 -0800

    debugging

--- app.js
+++ app.js
@@ -243,8 +243,10 @@ app.get('/incomingFile/:lineType/:line', function(req, res, next){
 			}
 		})
 
-
-		Result.find({experiment_id : '15'}, function(err, currUser){
+		console.log("expID: ");
+		console.log(expID);
+		console.log("want: '16'");
+		Result.find({experiment_id : expID}, function(err, currUser){
 			console.log('searching database');
 			if(currUser.length == 1){
 				console.log('user found');
